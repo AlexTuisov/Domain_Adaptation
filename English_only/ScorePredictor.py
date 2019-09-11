8aaba042dcf1ce2da119f6a592a94c5029dc1f2c
alex <queldelan@gmail.com>
2019-09-11 14:31:02 +0300

    Added neural net architecture and started writing the learning code

--- English_only/ScorePredictor.py
+++ English_only/ScorePredictor.py
@@ -4,12 +4,14 @@ import time
 import math
 import numpy as np
 import Preprocessing as pr
+import random
 from gensim.models.wrappers import FastText
 from sklearn.metrics import accuracy_score
 
 PATH_TO_DATA = "train.conllu"
 PATH_TO_MODEL = "cc.en.300.bin"
-NUM_OF_LAYERS = 2
+NUM_OF_LAYERS = 1
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def create_tensor(row_of_words, row_of_tags, model):
@@ -35,31 +37,79 @@ def scoring_function(self, state: np.array):
 
 
 class GRUPredictor(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, words_size, tags_size, tags_embedding_size):
         super(GRUPredictor, self).__init__()
-        self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.words_size = words_size
+        self.tags_size = tags_size
+        self.tags_embedding_size = tags_embedding_size
+        self.embedding = nn.Embedding(tags_size, tags_embedding_size)
+        hidden_size = words_size + tags_embedding_size
         self.gru = nn.GRU(hidden_size, hidden_size, num_layers=NUM_OF_LAYERS, batch_first=True)
-        self.linear = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.linear = nn.Linear(hidden_size, hidden_size)
+        self.loss = nn.MSELoss()
+        self.name = "my little net"
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input)
-        output, hidden = self.gru(embedded, hidden)
+    def forward(self, embedded_words, tags, hidden):
+        embedded_tags = self.embedding(tags)
+        gru_input = torch.cat([embedded_words, embedded_tags], 0)
+        output, hidden = self.gru(gru_input, hidden)
         output = self.linear(output[-1])
-        output = self.softmax(output)
+        output = self.loss(output)
         return output
 
     def initHidden(self, batch_size):
         return torch.zeros(NUM_OF_LAYERS, batch_size, self.hidden_size)
 
 
+def train_one_epoch(epoch_number, train_dataset, optimizer, model, criterion):
+    random_order = list(range(len(train_dataset)))
+    random.shuffle(random_order)
+    epoch_loss = 0
+    row_counter = 0
+    for i in random_order:
+        batch = train_dataset[i]
+        true_answers = [train_dataset[i][0][1]] * len(batch)
+        true_scores = map(calculate_true_score, zip(batch, true_answers))
+        output, loss = train_batch(optimizer, model, criterion, batch, true_scores)
+        epoch_loss += loss
+
+
+
+    average_loss = epoch_loss / row_counter
+    print(f"This epoch the average loss was {average_loss:.4f}")
+    return average_loss
+
+
+def train_batch(optimizer, model, criterion, batch, true_scores):
+    optimizer.zero_grad()
+    hidden = model.initHidden(batch.size()[0]).to(DEVICE)
+    output = model(batch, hidden)
+    loss = criterion(output, true_scores)
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 1000)
+    optimizer.step()
+    return output, loss.item()
+
+
+def calculate_true_score(sentence_and_tags, true_tags):
+    tags = sentence_and_tags[1]
+    length = len(tags)
+    assert length == len(true_tags)
+    count = 0
+    for i in range(length):
+        if tags[i] == true_tags[i]:
+            count += 1
+    return count/length
+
 if __name__ == '__main__':
+
+    # my_model = GRUPredictor(300, 80, 8)
+    # print(my_model.name)
+
     language_model = FastText.load_fasttext_format(PATH_TO_MODEL)
     x, y, tag_set = pr.load_train_test_validation_sets(PATH_TO_DATA)
     train_set = pr.WordsDataset(x, tag_set)
     test_set = pr.WordsDataset(y, tag_set)
-
 
     item = train_set[16][0]["words"]
     print(item)
